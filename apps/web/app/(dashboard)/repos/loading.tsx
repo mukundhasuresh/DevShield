7fd0d5d54573838766b9bfba0f1e747d93b523c5
mukundhasuresh <mukundhasuresh@gmail.com>
2026-04-03 00:20:41 +0530

    Create loading skeleton state for the repositories route

---
+++ apps/web/app/(dashboard)/repos/loading.tsx
@@ -0,0 +1,25 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+
+export default function ReposLoading() {
+  return (
+    <div className="flex flex-col space-y-6">
+      <div>
+        <Skeleton className="h-8 w-48 bg-[#222222]" />
+        <Skeleton className="h-4 w-72 bg-[#222222] mt-2" />
+      </div>
+      <Card className="bg-[#161616] border-[#222222] shadow-none">
+        <CardHeader>
+           <Skeleton className="h-5 w-40 bg-[#222222]" />
+        </CardHeader>
+        <CardContent>
+           <div className="space-y-4">
+            {[...Array(5)].map((_, i) => (
+              <Skeleton key={i} className="h-16 w-full bg-[#222222] rounded-md" />
+            ))}
+           </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
